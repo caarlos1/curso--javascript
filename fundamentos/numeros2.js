5d674561e3d9625b15eecc64cd8740505f24bf61
Carlos Roberto <crpu4a@gmail.com>
2021-03-13 11:59:06 -0300

    Algumas doideras do JavaScript e números.

---
+++ fundamentos/numeros2.js
@@ -0,0 +1,9 @@
+/*
+    Confira essas loucuras envolvendo números:
+*/
+
+console.log(7 / 0); // Retorna o que? Infinity
+console.log("10" / 2); // Erro? Não.. 5
+console.log("Text" * 2); // NaN
+console.log(0.1 + 0.7); // 0.7999999999999999 ??? É assim mesmo, é para ser mais rápido.+
+console.log( (15.23584584).toFixed(2) ); // Se tiver dentro de parenteses, funciona.
