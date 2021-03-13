4ec5f20677e203c12ad8f06d9f26cda62531de1f
Carlos Roberto <crpu4a@gmail.com>
2021-03-13 10:23:29 -0300

    Create organizacao.js

---
+++ fundamentos/organizacao.js
@@ -0,0 +1,8 @@
+console.log("Hello World my champs!");
+
+// Posso abrir blocos vazios. Para que? Não sei... Mas funciona. :)
+{ // Nome disso é chave... Não confunde com outro nome.
+    {
+        console.log("Estou dentro de um bloco.");
+    }
+}
