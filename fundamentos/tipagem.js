080dda1eb058ef6dbfa857f363a8a0cbe1564276
Carlos Roberto <crpu4a@gmail.com>
2021-03-13 11:58:26 -0300

    Aqui mostro a tipagem fraca do JS.

---
+++ fundamentos/tipagem.js
@@ -0,0 +1,20 @@
+/*
+    Já introduzindo, JavaScript tem uma tipagem fraca.
+    Então não se assute com o que irá acontecer ali em baixo.
+    Falows! ;x
+
+*/
+// Cria a variavel tipo let.
+let mensagem = "Uma simples string.";
+
+// type of retorna o tipo.
+console.log(mensagem);
+console.log(typeof mensagem);
+
+mensagem = 123456;
+// Será que vai?
+console.log(mensagem);
+console.log(typeof mensagem); // Foi!
+
+// Pera aí... Number?
+// É tipo isso.
