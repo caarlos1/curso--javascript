e76ac72c69f627c8704643965d3315c29d1afef0
Carlos Roberto <crpu4a@gmail.com>
2021-03-13 10:47:13 -0300

    Criação de var let const.

---
+++ fundamentos/varLetConst.js
@@ -0,0 +1,14 @@
+var a = 1; // Variavel normal, mas não precisa colocar na frente.
+let b = 2; // Não posso declarar de novo.
+const c = 3; // Não muda, se tentar  vai dar ruim.
+
+var a = 10; // Se fazer isso, funciona (não recomendável).
+// let b = 20; // Essa aqui da erro...
+// const c = 30 // Também não posso declarar novamente.
+
+a = 100;
+b = 200;
+// c = 300; // Vai dar erro.
+
+// Imprima os valores amigo console.
+console.log(a, b, c,);
