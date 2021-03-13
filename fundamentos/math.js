293a526cb66e2c0e33977a978d66e988515c2d0a
Carlos Roberto <crpu4a@gmail.com>
2021-03-13 13:37:19 -0300

    Um pouco sobre o objeto Math.

---
+++ fundamentos/math.js
@@ -0,0 +1,8 @@
+// Nosso querido Math é um objeto que usamos para fazer cálculos mais complicados.
+
+// Segue um exemplo bem simples.
+const raio = 5.6;
+const area = Math.PI * Math.pow (raio, 2); // PI é PI, pow é potência.
+
+console.log(area); // Resultado da brincadeira.
+console.log(typeof Math); // Math é um objeto.
