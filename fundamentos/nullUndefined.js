0ebbe6e2ac3a4aa0d4f32aa1314fa8400c9cdbcb
Carlos Roberto <crpu4a@gmail.com>
2021-03-13 21:03:47 -0300

    Nulo é diferente de não definido.

---
+++ fundamentos/nullUndefined.js
@@ -0,0 +1,10 @@
+let valorQualquer; //  Não inicializamos a variavel, apenas declaramos.
+console.log(valorQualquer); // Resultado: undefined
+
+valorQualquer = null; // Inicializamos com a auxencia de valor na memória.
+console.log(valorQualquer);
+
+/* 
+    Não definida (undefined) é != de Nulo (null).
+    Cuidado!!!!!
+*/
