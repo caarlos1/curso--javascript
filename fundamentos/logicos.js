840476e8792d8fca72b2e1b338cfa32ffe2d6301
Carlos Roberto <crpu4a@gmail.com>
2021-03-14 21:59:26 -0300

    Operadores.

---
+++ fundamentos/logicos.js
@@ -0,0 +1,22 @@
+/*
+and: &&
+    t && f = f
+    t && t = t
+    f && ? = f
+
+or: ||
+    t || f = t
+    t || t = t
+    f || ? = f 
+
+xor: != (gambiarra)
+    t xor f = t
+    t xor t = f
+    f xor t = t
+    f xor f = f
+
+negações: !
+    !t = f
+    !f = t
+
+*/
---
+++ fundamentos/ternario.js
@@ -0,0 +1,3 @@
+// O operador ternário abaixo é dividido em 3 partes ">=", "?" e ":".
+const resultado = nota => nota >= 7 ? "Aprovado" : "Reprovado" // retorno explícito
+console.log( resultado(7) )
