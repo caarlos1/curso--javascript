51d4e609b52770ca00ef5727e2efc1f1a2e95cfb
Carlos Roberto <crpu4a@gmail.com>
2021-03-16 22:33:30 -0300

    Tirei os colchetes das arrow function.

--- exercicios01/06.js
+++ exercicios01/06.js
@@ -9,14 +9,13 @@ let taxaDeJuros = 0.07;
 let tempoAplicacao = 2;
 
 
-const jurosSimples = (capitalInicial, taxaDeJuros, tempoAplicacao) => {
+const jurosSimples = (capitalInicial, taxaDeJuros, tempoAplicacao) => 
     console.log(`Montante (Juros Simples): R$${ ((capitalInicial + (( capitalInicial * taxaDeJuros ) * tempoAplicacao )).toFixed(2)).replace(".", ",") }`);
-}
 
-const jurosCompostos = (capitalInicial, taxaDeJuros, tempoAplicacao) => {
+const jurosCompostos = (capitalInicial, taxaDeJuros, tempoAplicacao) => 
     console.log(`Montante (Juros Compostos): R$${ (( capitalInicial * ( (1 + taxaDeJuros) ** tempoAplicacao)).toFixed(2)).replace(".", ",") } `);
-}
 
 console.log(`Investimento: ${capitalInicial}, Taxa de Juros: ${taxaDeJuros}, Tempo: ${tempoAplicacao}`);
+
 jurosSimples(capitalInicial, taxaDeJuros, tempoAplicacao)
 jurosCompostos(capitalInicial, taxaDeJuros, tempoAplicacao)
